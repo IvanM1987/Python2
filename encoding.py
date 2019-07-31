41b63db988d0de98769ca7eeb89155e8f221c658
IvanM1987 <jesmon1987@gmail.com>
2019-07-31 23:02:13 +0300

    lesson_1

---
+++ encoding.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+data_1 = 'декоратор'
+data_2 = 'сокет'
+data_3 = 'разработка'
+
+
+# In[2]:
+
+
+data_1.encode()
+
+
+# In[3]:
+
+
+data_2.encode()
+
+
+# In[4]:
+
+
+data_3.encode()
+
+
+# In[5]:
+
+
+a = b'class'
+a
+
+
+# In[6]:
+
+
+type(a)
+
+
+# In[7]:
+
+
+len(a)
+
+
+# In[8]:
+
+
+b = b'function'
+b
+
+
+# In[9]:
+
+
+type(b)
+
+
+# In[10]:
+
+
+len(b)
+
+
+# In[11]:
+
+
+c = b'method'
+c
+
+
+# In[12]:
+
+
+type(c)
+
+
+# In[13]:
+
+
+len(c)
+
+
+# In[14]:
+
+
+b'attribute'
+
+
+# In[15]:
+
+
+b'класс'
+
+
+# In[16]:
+
+
+b'функция'
+
+
+# In[34]:
+
+
+a = 'разработка'
+b = 'администрирование'
+c = 'protocol'
+d = 'standard'
+
+
+# In[35]:
+
+
+a = a.encode()
+a
+
+
+# In[36]:
+
+
+b = b.encode()
+b
+
+
+# In[37]:
+
+
+c = c.encode()
+c
+
+
+# In[38]:
+
+
+d = d.encode()
+d
+
+
+# In[39]:
+
+
+a.decode()
+
+
+# In[41]:
+
+
+b.decode()
+
+
+# In[42]:
+
+
+c.decode()
+
+
+# In[43]:
+
+
+d.decode()
+
+
+# In[ ]:
+
+
+
+
